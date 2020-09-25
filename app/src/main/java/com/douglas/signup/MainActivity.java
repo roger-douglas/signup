e2fcf8d545a16442cefe7d8886fc830ce84e1f52
petruki <31597636+petruki@users.noreply.github.com>
2020-09-25 00:30:57 -0700

    Added secret value

--- app/src/main/java/com/douglas/signup/MainActivity.java
+++ app/src/main/java/com/douglas/signup/MainActivity.java
@@ -4,18 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnSignUp;
+    TextView textSecret;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textSecret = (TextView) findViewById(R.id.textSecret);
+        textSecret.setText(BuildConfig.SECRET);
         buildSignUp();
     }
 
